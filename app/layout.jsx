5c4103745544b789e509241dc89d16bd75771227
Chris <495032732@qq.com>
2023-11-07 23:38:37 +0100

    first time

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: "promptopia",
+    description: 'Discover % Share Valuable AI Prompts'
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient'/>
+            </div>
+            
+            <main className='app'>
+                {children}
+            </main>
+
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
